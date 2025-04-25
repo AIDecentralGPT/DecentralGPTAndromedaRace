5d4033f0775637b450f5f676b49f5dc706eb6f3a
okokrrr <rustokok@gmail.com>
2025-04-25 11:17:56 +0800

    delete unuse

--- interfaces/IRentContract.sol
+++
@@ -1,8 +0,0 @@
-
-interface IRentContract {
-    function getTotalBurnedRentFee(uint8 phaseLevel) external view returns (uint256);
-
-    function getTotalRentedGPUCount(uint256 phaseLevel) external view returns (uint256);
-
-    function isRented(string calldata machineId) external view returns (bool);
-}
